ea76374320221970d80d21c645da7962d2d4b48c
diegocamy <53247032+diegocamy@users.noreply.github.com>
2019-12-01 16:13:29 -0300

    Updates

---
+++ precache-manifest.43b4d9e5153a49e87c90b734a95275c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e2b766fd0be8e5f59c9fd73c195cb5",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "433014b4e4c165a750a7",
+    "url": "/todo-app/static/css/main.5b607011.chunk.css"
+  },
+  {
+    "revision": "2c09d76fc5f0e400feae",
+    "url": "/todo-app/static/js/2.f0d3a264.chunk.js"
+  },
+  {
+    "revision": "433014b4e4c165a750a7",
+    "url": "/todo-app/static/js/main.cf397586.chunk.js"
+  },
+  {
+    "revision": "14566414055b74d3ad79",
+    "url": "/todo-app/static/js/runtime-main.c8041210.js"
+  }
+]);
